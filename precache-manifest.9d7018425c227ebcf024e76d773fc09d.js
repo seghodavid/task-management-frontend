29721e8894f4cf71802e84162b709ea0b4d88d52
seghodavid <seghodavid@users.noreply.github.com>
2022-06-07 20:38:45 +0000

    deploy: 995be5cd12d1230b654ac20a9e5c750a4c9b42ad

---
+++ precache-manifest.9d7018425c227ebcf024e76d773fc09d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9ca1e0fb454b70092d63729e718c96",
+    "url": "https://seghodavid.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "b9cfe36a13dcf2f1a078",
+    "url": "https://seghodavid.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "7cbcd19ab0a3e096b2c2",
+    "url": "https://seghodavid.github.io/task-management-frontend/static/js/2.5953827d.chunk.js"
+  },
+  {
+    "revision": "b9cfe36a13dcf2f1a078",
+    "url": "https://seghodavid.github.io/task-management-frontend/static/js/main.606e5fdf.chunk.js"
+  },
+  {
+    "revision": "61c2798ada9f848b79f6",
+    "url": "https://seghodavid.github.io/task-management-frontend/static/js/runtime~main.da13b1cf.js"
+  }
+]);
